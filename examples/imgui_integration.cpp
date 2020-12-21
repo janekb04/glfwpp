23ea569834072575d78a15d4550b6b6569698d64
janekb04 <janekb04@icloud.com>
2020-12-21 23:33:24 +0100

    Add ImGui integration example

---
+++ examples/imgui_integration.cpp
@@ -0,0 +1,77 @@
+#include <GL/glew.h>
+#include <cmath>
+#include <glfwpp/glfwpp.h>
+#include <imgui.h>
+#include <imgui_impl_glfw.h>
+#include <imgui_impl_opengl3.h>
+
+template <typename Func>
+void renderImgui(Func&& guiRenderFunc_)
+{
+    ImGui_ImplOpenGL3_NewFrame();
+    ImGui_ImplGlfw_NewFrame();
+    ImGui::NewFrame();
+
+    guiRenderFunc_();
+
+    ImGui::Render();
+    ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+    if (ImGui::GetIO().ConfigFlags & ImGuiConfigFlags_ViewportsEnable)
+    {
+        glfw::Window& backupCurrentContext = glfw::getCurrentContext();
+        ImGui::UpdatePlatformWindows();
+        ImGui::RenderPlatformWindowsDefault();
+        glfw::makeContextCurrent(backupCurrentContext);
+    }
+}
+
+void initImgui(const glfw::Window& wnd)
+{
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGuiIO& io = ImGui::GetIO();
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;
+    io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;
+    io.ConfigFlags |= ImGuiConfigFlags_ViewportsEnable;
+
+    ImGuiStyle& style = ImGui::GetStyle();
+    style.WindowRounding = 0.0f;
+    style.Colors[ImGuiCol_WindowBg].w = 1.0f;
+
+    ImGui_ImplGlfw_InitForOpenGL(wnd, true);
+    ImGui_ImplOpenGL3_Init();
+}
+
+int main()
+{
+    glfw::GlfwLibrary library = glfw::init();
+
+    glfw::WindowHints hints;
+    hints.clientApi = glfw::ClientApi::OpenGl;
+    hints.contextVersionMajor = 4;
+    hints.contextVersionMinor = 6;
+    hints.apply();
+    glfw::Window wnd(800, 600, "GLFWPP ImGui integration example");
+
+    glfw::makeContextCurrent(wnd);
+    if(glewInit() != GLEW_OK)
+    {
+        throw std::runtime_error("Could not initialize GLEW");
+    }
+
+    initImgui(wnd);
+
+    while(!wnd.shouldClose())
+    {
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        renderImgui([](){
+           ImGui::ShowDemoWindow();
+        });
+
+        glfw::pollEvents();
+        wnd.swapBuffers();
+    }
+}

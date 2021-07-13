fab3f26aaaaed40be027659f2eae114a3f166e9d
Jan Bielak <janekb04@icloud.com>
2021-07-13 21:03:08 +0200

    Add [[maybe_unused]] attr to GLFWlibrary object

--- examples/imgui_integration.cpp
+++ examples/imgui_integration.cpp
@@ -53,7 +53,7 @@ void initImgui(const glfw::Window& wnd)
 
 int main()
 {
-    glfw::GlfwLibrary library = glfw::init();
+    [[maybe_unused]] glfw::GlfwLibrary library = glfw::init();
 
     glfw::WindowHints hints;
     hints.clientApi = glfw::ClientApi::OpenGl;

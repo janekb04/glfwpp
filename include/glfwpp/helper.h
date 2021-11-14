c51bc6418d572199adc8200fdeeb43f3cab5ef64
Jan Bielak <contact@janbielak.com>
2021-11-14 19:28:15 +0100

    Add helper OwningPtr class
    Signed-off-by: Jan Bielak <contact@janbielak.com>

--- include/glfwpp/helper.h
+++ include/glfwpp/helper.h
@@ -21,4 +21,51 @@
         return static_cast<std::underlying_type_t<Enum>>(lhs) ^ static_cast<std::underlying_type_t<Enum>>(rhs); \
     }
 
+namespace glfw
+{
+    namespace detail
+    {
+        template<typename T>
+        class OwningPtr
+        {
+        private:
+            T* _ptr;
+
+        public:
+            OwningPtr(const OwningPtr&) = delete;
+            OwningPtr& operator=(const OwningPtr&) = delete;
+
+            constexpr OwningPtr(std::nullptr_t = nullptr) noexcept :
+                _ptr{}
+            {
+            }
+
+            constexpr OwningPtr(T* ptr_) :
+                _ptr{ptr_}
+            {
+            }
+
+            constexpr OwningPtr(OwningPtr&& other) noexcept :
+                _ptr{std::exchange(other._ptr, nullptr)}
+            {
+            }
+
+            constexpr OwningPtr& operator=(OwningPtr&& other) noexcept
+            {
+                _ptr = std::exchange(other._ptr, nullptr);
+            }
+
+            [[nodiscard]] constexpr operator bool() const noexcept
+            {
+                return static_cast<bool>(_ptr);
+            }
+
+            [[nodiscard]] constexpr operator T*() const noexcept
+            {
+                return _ptr;
+            }
+        };
+    }  // namespace detail
+}  // namespace glfw
+
 #endif  //GLFWPP_HELPER_H
